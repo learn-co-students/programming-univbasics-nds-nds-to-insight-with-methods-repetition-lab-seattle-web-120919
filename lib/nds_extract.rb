a830713f3b07b5255182f96085794dcb68828b70
honorable-exception-1170 <aliciachao@gmail.com>
2019-12-08 00:26:28 +0000

    Done.

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -1,7 +1,7 @@
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 require_relative './directors_database'
 
-def directors_totals(source) # AoHash of { dir_name => gross }
+def directors_totals(source) # input gross_for_director to AoH: { dir_name => gross }
   result = {}
   director_index = 0
   while director_index < source.size do
@@ -12,7 +12,8 @@ def directors_totals(source) # AoHash of { dir_name => gross }
   result
 end
 
-def gross_for_director(d) # $$ per director
+def gross_for_director(d) # calculate gross per director
+  # d = already drilled down into first index.
   total = 0
   index = 0
 
@@ -27,13 +28,13 @@ def list_of_directors(source) # Array of directors names
   names_index = 0
   names = []
   while names_index < source.length do
-    names << source[names_index][:name]
+    names << source[names_index][:name] #append(shovel) string to array
     names_index += 1
   end
   names
 end
 
-def total_gross(source)
+def total_gross(source) #loop thru list_of_directors to access AoH directors_totals to total all gross
 grand_total = 0
 i = 0
   while i < source.size do
